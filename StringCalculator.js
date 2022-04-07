4d2668872d00c9fbcdf16a9128e15b684a5f10d1
justin-s <justin.s@kakaocorp.com>
2022-04-07 16:21:31 +0900

    분기하는 로직

--- StringCalculator.js
+++ StringCalculator.js
@@ -1,6 +1,27 @@
 const StringCalculator = (args) => {
     const inputs = args.split(" ");
 
+    const calculator = (number, input) => {
+        if (number === 0) return Number.parseInt(input[number]);
+
+        return branchMethod(number, input);
+    };
+
+    const branchMethod = (number, input) => {
+        switch (input[number]) {
+            case "+":
+                return add(number, input);
+            case "-":
+                return subtract(number, input);
+            case "*":
+                return multiply(number, input);
+            case "/":
+                return divide(number, input);
+            default:
+                return number;
+        }
+    };
+
 
 };
 
