d259d1e7364a466d11bad1c9eafa42735948b779
justin-s <justin.s@kakaocorp.com>
2022-04-07 15:33:29 +0900

    계산기 로직

--- StringCalculator.java
+++ StringCalculator.java
@@ -11,5 +11,32 @@ public class StringCalculator {
             return Integer.parseInt(input[number]);
         }
 
+        return branchMethod(number, input);
+    }
+
+    private int branchMethod(int number, String[] input) {
+        switch (input[number]) {
+            case "+" : return add(number, input);
+            case "-" : return subtract(number, input);
+            case "*" : return multiply(number, input);
+            case "/" : return divide(number, input);
+            default: return number;
+        }
+    }
+
+    private int add(int number, String[] input) {
+        return calculator(number -1, input) + Integer.parseInt(input[number + 1]);
+    }
+
+    private int subtract(int number, String[] input) {
+        return calculator(number -1, input) - Integer.parseInt(input[number + 1]);
+    }
+
+    private int divide(int number, String[] input) {
+        return calculator(number -1, input) / Integer.parseInt(input[number + 1]);
+    }
+
+    private int multiply(int number, String[] input) {
+        return calculator(number -1, input) * Integer.parseInt(input[number + 1]);
     }
 }
